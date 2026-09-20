082e1cb94fb6c13c2e4a8c482eab5acec839c82d
roli-lpci <rbosch@lpci.ai>
2026-09-19 21:09:53 -0400

    langstate 0.2.4

--- Formula/langstate.rb
+++ Formula/langstate.rb
@@ -3,8 +3,8 @@ class Langstate < Formula
 
   desc "Inspect and verify compressed LLM conversation state"
   homepage "https://hermes-labs.ai/langstate"
-  url "https://files.pythonhosted.org/packages/dc/3d/610c67497a5c8880572706405edb147a808478ee1afa3753c9e2744e48a8/langstate-0.2.3.tar.gz"
-  sha256 "f1e373978630788a82719811a97432a6fa2b0bfbc2c6abb3988b772fc078daef"
+  url "https://files.pythonhosted.org/packages/8e/66/4aed97c10f4bfbedb347f169acf8db68117cc152c314ebdeb83cb5c4a1de/langstate-0.2.4.tar.gz"
+  sha256 "0c0cf4252cf803c1146ced17c5727e77aaabd06ad3b7b58adb2a9f8ea7be33fd"
   license "Apache-2.0"
 
   depends_on "python@3.13"
